72e1b3b518eeec376b5269f0c5013bf34fd4d855
Oliver <Oliverswater@gmail.com>
2024-06-23 16:56:57 +0100

    new query

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -2,6 +2,16 @@ import "server-only";
 import { db } from "./index";
 import { cookies } from "next/headers";
 
+/** takes input of a 32-character UUID and returns true or false*/
+export async function hasUserPlayed(userUuid: string) {
+  const user = await db.query.users.findFirst({
+    where: (model, { eq }) => eq(model.id, userUuid),
+  });
+
+  if (!user) return false;
+  return true;
+}
+
 export async function getTop5VotesMonth() {
   return await db.query.votes.findMany({
     orderBy: (votes, { asc }) => [asc(votes.votesThisMonth)],
