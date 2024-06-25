733fab701bce73011183ae0a599e1e20b5bc8ecc
Oliver <Oliverswater@gmail.com>
2024-06-25 05:11:18 +0100

    prog

--- src/app/_components/signinbutton.tsx
+++ src/app/_components/signinbutton.tsx
@@ -9,6 +9,16 @@ import { SignInForm } from "./signinform";
 import { cookies } from "next/headers";
 
 export function SignInButton() {
+  const userCookie = cookies().get("userUuid");
+
+  if (userCookie?.value) {
+    const user = userCookie.value;
+    return (< className="flex items-center space-x-2">
+      <span className="text-white">Signed in as {user}</span>
+      <button onClick={() => cookies().delete("userUuid")} className="text-white">Sign Out</button>
+    </div>);
+  }
+  }
   return (
     <Dialog>
       <DialogTrigger className="px-4">Sign In</DialogTrigger>
